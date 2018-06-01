a3fad98636d77b474fa431b203b9436b530b3a38
aduo122 <ytarch122@gmail.com>
2018-06-01 00:09:25 -0400

    update

--- node/index.js
+++ node/index.js
@@ -1,46 +1,46 @@
-var argv = require('minimist')(process.argc.slice(2));
+var argv = require('minimist')(process.argv.slice(2));
 var port = argv['port'];
 var redis_host = argv['redis_host'];
 var redis_port = argv['redis_port'];
 var redis_channel = argv['redis_channel'];
 
+// Setup dependency instances.
 var express = require('express');
 var app = express();
-var server = requeire('http').createServer(app);
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-var redis = requeire('redis');
+// Setup redis client.
+var redis = require('redis');
 console.log('Creating a redis client');
-var redisClient = redis.createServer(redis_host, redis_port);
+var redisClient = redis.createClient(redis_port, redis_host);
 console.log('Subscribing to redis topic: %s', redis_channel);
-redisClient.subscribe(redis_channel);
-redisClient.on('message', function(channel, message)){
-    if (channel == redis_channel){
-        console.log('message received %s', message);
-        io.sockets.emit('data', message);
-    }
-};
-
-app.use(express.static(__dirname + '/public'));
-app.use('/jquery', express.satic(__dirname));
-app.use('/d3', express.static(__dirname + '/node_modules/d3/'));
-app.use('/nvd3', express.static(__dirname + '/node_modules/nvd3/'));
-app.use('/bootstrap', express.static(__dirname + '/node_modules/d3/'));
-
-server.listen(port, function(){
-    console.log('Server started at port %d', port);
+redisClient.subscribe(redis_channel)
+redisClient.on('message', function(channel, message) {
+	if (channel == redis_channel) {
+		console.log('message received %s', message);
+		io.sockets.emit('data', message)
+	}
 });
 
-var shutdown_hook = function(){
+// Setup webapp routing.
+app.use(express.static(__dirname + '/public'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
+app.use('/d3', express.static(__dirname + '/node_modules/d3/'));
+app.use('/nvd3', express.static(__dirname + '/node_modules/nvd3/build/'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+
+server.listen(port, function() {
+	console.log('Server started at port %d', port);
+})
+
+var shutdown_hook = function () {
     console.log('Quitting redis client');
     redisClient.quit();
     console.log('Shutting down app');
-
+    process.exit();
 };
 
-
-
-
-
-
-
+process.on('SIGTERM', shutdown_hook)
+process.on('SIGINT', shutdown_hook)
+process.on('exit', shutdown_hook)
--- node/public/main.js
+++ node/public/main.js
@@ -1,42 +1,72 @@
-$(function(){
-    var data_points = [];
-    data_points.push({values: [], key: 'BTC-USD'});
+$(function() {
+	var data_points = [];
+	data_points.push({values: [], key: 'BTC-USD'});
 
-    $('chart').height($(window).height() - $('#header').height() *2);
+	$('#chart').height($(window).height() - $('#header').height() * 2);
 
-    var chart = nv.models.lineChart()
-        .interpolate('monotone')
-        .margin({botton:100})
-        .useInteractiveGuidelinbe(true)
-        .showLegend(true)
-        .color(d3.scale.category10().range());
+	var chart = nv.models.lineChart()
+		.interpolate('monotone')
+		.margin({botton:100})
+		.useInteractiveGuideline(true)
+		.showLegend(true)
+		.color(d3.scale.category10().range());
 
-    chart.xAxis
-        .axisLabel('Time')
-        .tickFormat(formatDateTick);
+	chart.xAxis
+		.axisLabel('Time')
+		.tickFormat(formatDateTick);
 
-    chart.yAxis
-        .axisLabel('Price');
+	chart.yAxis
+		.axisLabel('Price');
 
-    nv.addGraph(loadGraph);
-
-    function loadGraph(){
-        d3.select('#chart svg')
-            .dartum(data_points)
-            .transistion()
-            .duration(5)
-            .call(chart)
-        nv.utils.windowResize(chart.update);
-        return chart;
-    }
-
-    function formatDateTick(time){
-        var date = new Date(time);
-    }
+	nv.addGraph(loadGraph);
 
 
+	function loadGraph() {
+		d3.select('#chart svg')
+			.datum(data_points)
+			.transition()
+			.duration(5)
+			.call(chart);
 
+		nv.utils.windowResize(chart.update);
+		return chart;
+	}
 
+	function formatDateTick(time) {
+		var date = new Date(time);
+		console.log(time);
+		return d3.time.format('%H:%M:%S')(date);
+	}
 
+	function newDataCallback(message) {
+		var parsed = JSON.parse(message);
+		var timestamp = parsed['Timestamp'];
+		var price = parsed['Average'];
+		var symbol = parsed['Symbol'];
+		var point = {};
+		point.x = timestamp;
+		point.y = price;
 
+		var i = getSymbolIndex(symbol, data_points);
+		data_points[i].values.push(point);
+		if (data_points[i].values.length > 100) {
+			data_point[i].values.shift();
+		}
+		loadGraph();
+	}
+
+	function getSymbolIndex(symbol, array) {
+		for (var i = 0; i < array.length; i++) {
+			if (array[i].key == symbol) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	var socket = io();
+
+	socket.on('data', function(data) {
+		newDataCallback(data);
+	})
 });
